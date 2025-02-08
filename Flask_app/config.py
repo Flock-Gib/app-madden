27521cf014388df38295e4dd9b27a624f0d881ee
Flock-Gib <gibsonst1993@gmail.com>
2025-02-07 22:29:26 -0500

    Delete Flask_app directory

--- Flask_app/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'your_secret_key')
-    BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', f'sqlite:///{os.path.join(BASE_DIR, "users.db")}')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', 'your_stripe_secret_key')
